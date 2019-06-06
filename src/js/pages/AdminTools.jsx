728591413b4634bbf048c2b85c2b5320c3a9e46e
alexanderevgrafov <alexander.evgrafov@gmail.com>
2019-06-07 00:58:37 +0300

    Add new libs + AdminTools

---
+++ src/js/pages/AdminTools.jsx
@@ -0,0 +1,117 @@
+import React from 'react-type-r';
+import { Record, define, type } from 'type-r';
+import { Route, Switch } from 'react-router';
+import { Badge, Col, Container, Form, Row } from 'react-bootstrap';
+import { _t } from 'app/translate';
+import { papers, templates } from 'templates/all_server';
+import { Slider } from 'ui/controls/Slider';
+import { PreparePage } from './Prepare';
+import { FormRow } from 'ui/Controls';
+import cx from 'classnames';
+import { InPost } from 'models/InModels';
+import { json as examplePost } from './ExamplePost'
+
+const PAPER_SIZE = 'a5';
+
+@define
+class TmplTesterModel extends Record {
+    static attributes = {
+        tmpl        : type( String ).has.watcher( 'onTmplChange' ),
+        padding     : type( Number ).value( 10 ).has.watcher( 'onPadding' ),
+        font_size   : type( Number ).value( 100 ).has.watcher( 'onFontSize' ),
+        posts       : InPost.Collection,
+        text_length : 100
+    };
+
+    onPadding() {
+        this.posts.each( post => post.config.page_padding = this.padding );
+    }
+
+    onFontSize() {
+        this.posts.each( post => post.config.post_font_size = this.font_size );
+    }
+
+    /* onPicsCount() {
+     }*/
+
+    onTmplChange() {
+        this.posts.each( post => post.config.tmpl0 = this.tmpl );
+    }
+}
+
+@define
+export default class TmplTester extends React.Component {
+    static state = TmplTesterModel;
+
+    componentWillMount() {
+        this.state.tmpl = templates.at( 0 ).name;
+
+        const models =
+                  _.map( _.range( 0, 11 ), num => {
+                      const model = new InPost();
+
+                      model.set( examplePost, { parse : true } );
+                      model.id = num;
+                      model.media.pictures.reset( model.media.pictures.slice( 0, num ) );
+                      model.config = {
+                          tmpl0          : this.state.tmpl0,
+                          page_padding   : this.state.padding,
+                          post_font_size : this.state.font_size
+                      };
+                      return model;
+                  } );
+
+        this.state.posts.reset( models, { merge : true } );
+    }
+
+    render() {
+        const { state } = this,
+              scale     = .5,
+              { posts } = state;
+
+        return <Container>
+            <Row>
+                <Col>
+                    <FormRow label='Template'>
+                        <Form.ControlLinked as='select' valueLink={state.linkAt( 'tmpl' )}>
+                            {templates.map( t => <option value={t.name} key={t.name}>{t.native || t.name}</option> )}
+                        </Form.ControlLinked>
+                    </FormRow>
+                    <FormRow label={'Padding (' + state.padding + ')'}>
+                        <Slider valueLink={state.linkAt( 'padding' )} min={0} max={100}/>
+                    </FormRow>
+                    <FormRow label={'Font size (' + (state.font_size / 100) + 'em)'}>
+                        <Slider valueLink={state.linkAt( 'font_size' )} min={20} max={400}/>
+                    </FormRow>
+                    <FormRow label={'Text length (' + state.text_length + '%)'}>
+                        <Slider valueLink={state.linkAt( 'text_length' )} min={0} max={100}/>
+                    </FormRow>
+                </Col>
+            </Row>
+            <Row>
+                {posts.map( post => {
+
+                        const
+                            tmpl0   = templates.get( post.config.tmpl0 ),
+
+                            ThePage = tmpl0 ? tmpl0.page( post, PAPER_SIZE ) : null;
+
+                        return <div className={cx( 'prepare_preview_box', 'admin_tools_preview', PAPER_SIZE )}
+                                    style={scale < 1 ? { transform : 'scale(' + scale + ')' } : null}>
+                            <div className='prepare_preview_page side1'>{ThePage}</div>
+                        </div>;
+                    }
+                )}
+            </Row>
+        </Container>;
+    }
+}
+
+@define
+export default class AdminTools extends React.Component {
+    render() {
+        return <Switch>
+            <Route path='/admin/tmpltest' component={TmplTester}/>
+        </Switch>;
+    }
+}
--- src/js/ui/Bootstrap.jsx
+++ src/js/ui/Bootstrap.jsx
@@ -9,6 +9,19 @@ import cx from 'classnames'
 const Form = BsForm,
       Card = BsCard;
 
+/*
+FormRow= ({label, units, children, ...props}) =>
+    <BsForm.Row { ...props }>
+        { label !== undefined && <div  className='form_row_label'>{ label }</div> }
+        <div className='form_row_body'>
+            <div className='form_row_control'>
+                { children }
+            </div>
+            { units && <div className='form_row_units'>{ units }</div> }
+        </div>
+    </BsForm.Row>;
+*/
+
 Form.ControlLinked = ( { valueLink, onChange, accepts, value, ...props } ) =>
     <BsForm.Control
         onChange={e => {
@@ -28,6 +41,8 @@ Card.SquareImg = ( { src, className, ...props } ) =>
         style={{ backgroundImage : 'url(' + src + ')' }}
         {...props}
     />;
+
+
 
 const MyButton = ( { label, children, ...props } ) =>
     <BsButton {...props}>{label || children}
